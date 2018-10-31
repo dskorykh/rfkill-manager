2ffa5c6d753b62b9492a9b8c7c5f25da8db53a24
Dmitry Skorykh <dmitry.skorykh@emlid.com>
2018-10-31 14:09:35 +0300

    examples: Add simple example to execute rfkill work

---
+++ examples/rfkill_example.py
@@ -0,0 +1,15 @@
+import rfkill_manager
+
+from pprint import pprint
+
+
+def get_rfkill_list():
+    return rfkill_manager.rfkill_list()
+
+
+if __name__ == '__main__':
+    pprint(get_rfkill_list())
+    rfkill_manager.rfkill_execute_event_by_type('bluetooth', 'block')
+    pprint(get_rfkill_list())
+    rfkill_manager.rfkill_execute_event_by_type('bluetooth', 'unblock')
+    pprint(get_rfkill_list())
